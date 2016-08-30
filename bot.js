eabcf7db4d0dbbbb81dc3e6a1ebdef888dc2d76a
Michal Brcak <michal.brcak@gmail.com>
2016-08-30 08:25:58 +0200

    Bot Fix
    getForecast

--- bot.js
+++ bot.js
@@ -75,21 +75,6 @@ const actions = {
     context.forecast = 'sunny';
     cb(context);
   }, 
-  
-  
-  getForecast({context, entities}) {
-  return new Promise(function(resolve, reject) {
-    var location = firstEntityValue(entities, "location")
-    if (location) {
-      context.forecast = 'sunny in ' + location; // we should call a weather API here
-      delete context.missingLocation;
-    } else {
-      context.missingLocation = true;
-      delete context.forecast;
-    }
-    return resolve(context);
-  });
-},
 };
 
 
