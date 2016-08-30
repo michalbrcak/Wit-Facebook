668461dee47874f938dbb28eecb82f4c02b4b1aa
Michal Brcak <michal.brcak@gmail.com>
2016-08-30 02:13:05 +0200

    Added forecast function
    testing bot

--- bot.js
+++ bot.js
@@ -74,7 +74,22 @@ const actions = {
     // context.forecast = apiCall(context.loc)
     context.forecast = 'sunny';
     cb(context);
-  },
+  }, 
+  
+  
+  getForecast({context, entities}) {
+  return new Promise(function(resolve, reject) {
+    var location = firstEntityValue(entities, "location")
+    if (location) {
+      context.forecast = 'sunny in ' + location; // we should call a weather API here
+      delete context.missingLocation;
+    } else {
+      context.missingLocation = true;
+      delete context.forecast;
+    }
+    return resolve(context);
+  });
+},
 };
 
 
