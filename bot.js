365d4b4881dd0269ee49d9e1903699a66c5ef6f4
Michal Brcak <michal.brcak@gmail.com>
2016-11-03 00:38:13 +0100

    v2

--- bot.js
+++ bot.js
@@ -75,6 +75,24 @@ const actions = {
     context.forecast = 'slneèno';
     cb(context);
   }, 
+  
+  send(request, response) {
+    const {sessionId, context, entities} = request;
+    const {text, quickreplies} = response;
+    return new Promise(function(resolve, reject) {
+      console.log('sending...', JSON.stringify(response));
+      return resolve();
+    });
+  },
+  getForecast({context, entities}) {
+    return new Promise(function(resolve, reject) {
+      // Here should go the api call, e.g.:
+      // context.forecast = apiCall(context.loc)
+      context.forecast = 'sunny';
+      return resolve(context);
+    });
+  },
+  
 };
 
 
