226b3dba908be8774f7a7d0f9afce756c690d827
michalbrcak <michal.brcak@gmail.com>
2016-11-03 00:47:47 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -72,27 +72,9 @@ const actions = {
   ['fetch-weather'](sessionId, context, cb) {
     // Here should go the api call, e.g.:
     // context.forecast = apiCall(context.loc)
-    context.forecast = 'slneËno';
+    context.forecast = 'slneƒçno';
     cb(context);
-  }, 
-  
-  send(request, response) {
-    const {sessionId, context, entities} = request;
-    const {text, quickreplies} = response;
-    return new Promise(function(resolve, reject) {
-      console.log('sending...', JSON.stringify(response));
-      return resolve();
-    });
-  },
-  getForecast({context, entities}) {
-    return new Promise(function(resolve, reject) {
-      // Here should go the api call, e.g.:
-      // context.forecast = apiCall(context.loc)
-      context.forecast = 'sunny';
-      return resolve(context);
-    });
-  },
-  
+  },   
 };
 
 
